14cd77a49e502748a7d5c83efa70a0997d433a1c
danhdezv <dan.hdez.v@gmail.com>
2018-05-24 21:03:43 -0500

    Practica del dia

--- js/Other2.js
+++ js/Other2.js
@@ -1,2 +1,20 @@
 Este si tiene contenido
 
+
+
+
+jkdjdj
+
+Agregar 2 imagenes al folder de img
+registrar en la historia solo una imagen
+crea un archivo llamado "lala.hmtl" en la raiz del pryecto
+registrar en la historia la creacion del archivo
+renombra el archivo lala.hmlt para que se llame "contact..hmtl "
+agregar contenido en elarchivo contact.html" con su nombre y tu cuenta de github 
+en el archivo "index.html" escribe el texto 
+<a href="contact.html">contact </a>
+asegurate que todos tus cambios (expeto la segunda imagen )  ya esten en la historia y rriba en github
+
+
+
+
